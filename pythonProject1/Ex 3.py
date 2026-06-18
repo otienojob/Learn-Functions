2020f6f986579915cd5058887222da81adc6af0d
Job Otian'ga <114091849+otienojob@users.noreply.github.com>
2026-06-18 16:03:18 +0300

    Commit Changes - 2

--- pythonProject1/Ex 3.py
+++ pythonProject1/Ex 3.py
@@ -1,11 +1,12 @@
-Exam={}
+studentProfile={}
 
 while True:
     print("---Menu Options---")
     print(f"1. Add Student Details: \n")
-    print(f"2. Update Student Details: \n")
-    print(f"3. Delete Student Details: \n")
-    print(f"4. Exit: \n)")
+    print(f"2. View Student Profile: \n")
+    print(f"3. Update Student Details: \n")
+    print(f"4. Delete Student Details: \n")
+    print(f"5. Exit: \n)")
 
     menu_pick=input(f"Select action item: ").strip()
     if menu_pick == "1":
@@ -23,3 +24,11 @@ while True:
             if studentGender in ["M","F","Male","Female"]
             break
         print(f"Enter a valide gender entry - M or F or Male or Female ")
+        studentProfile={
+            "Student Names":student_name,
+            "Student Age":student_age,
+            "Student Gender":studentGender
+            }
+        print(f"You have successfully created the student's profile")
+    elif menu_pick=="2":
+        print(studentProfile)
