2e5f43ff6bace449e3e8b78a70ecac92efc1ebac
Job Otian'ga <114091849+otienojob@users.noreply.github.com>
2026-06-19 11:36:20 +0300

    Updated code

--- pythonProject1/Ex 3.py
+++ pythonProject1/Ex 3.py
@@ -7,28 +7,35 @@ while True:
     print(f"3. Update Student Details: \n")
     print(f"4. Delete Student Details: \n")
     print(f"5. Exit: \n)")
-
-    menu_pick=input(f"Select action item: ").strip()
+#Select user input
+    
+    menu_pick=input(f"Select a menu item: ").strip()
     if menu_pick == "1":
-        student_name=input(f"Enter Student's Name - First & Second Name \n").strip()
-        if student_name:
-            break
-        print("Name cannot be empty")
+        student_name=input(f"Enter Student's Name - First & Second Name \n").strip().upper()
+        #if student_name:
+        #    break
+        #print("Name cannot be empty")
 
         while True:
             student_age=int(input(f"Enter Student's Age: \n").strip())
-            break
-        print(f"The age has to be an integer!")
+            if student_age:
+                break
+            print(f"Value cannot be empty or null!")
+
         while True:
             studentGender=input(f"Enter the Student's gender: \n").strip().upper()
             if studentGender in ["M","F","Male","Female"]:
                 break
-        print(f"Enter a valide gender entry - M or F or Male or Female ")
+            print(f"Enter a valide gender entry - M or F or Male or Female ")
+        #store dictionary details
+                
         studentProfile={
             "Student Names":student_name,
             "Student Age":student_age,
             "Student Gender":studentGender
             }
         print(f"You have successfully created the student's profile")
+        print(studentProfile)
+
     elif menu_pick=="2":
         print(studentProfile)
