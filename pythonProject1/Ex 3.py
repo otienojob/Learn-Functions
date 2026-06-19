168b8e85b3fa1bab22491fbf3d16d77cc69a868c
Job Otian'ga <114091849+otienojob@users.noreply.github.com>
2026-06-19 12:23:23 +0300

    Improved code

--- pythonProject1/Ex 3.py
+++ pythonProject1/Ex 3.py
@@ -4,27 +4,31 @@ while True:
     print("---Menu Options---")
     print(f"1. Add Student Details: \n")
     print(f"2. View Student Profile: \n")
-    print(f"3. Update Student Details: \n")
-    print(f"4. Delete Student Details: \n")
-    print(f"5. Exit: \n)")
+    print(f"3. Exit: ")
 #Select user input
     
     menu_pick=input(f"Select a menu item: ").strip()
+
     if menu_pick == "1":
-        student_name=input(f"Enter Student's Name - First & Second Name \n").strip().upper()
-        #if student_name:
-        #    break
-        #print("Name cannot be empty")
-
         while True:
-            student_age=int(input(f"Enter Student's Age: \n").strip())
-            if student_age:
+            student_name=input(f"Enter Student's Name - First & Second Name \n").strip().title()
+            
+            if student_name:
                 break
-            print(f"Value cannot be empty or null!")
+            print("Student Name cannot be empty")
 
         while True:
-            studentGender=input(f"Enter the Student's gender: \n").strip().upper()
-            if studentGender in ["M","F","Male","Female"]:
+            try:
+                student_age=int(input(f"Enter Student's Age: \n").strip())
+                if student_age > 0:
+                    break
+                print(f"Value cannot be less than zero!")
+            except ValueError:
+                print(f"Value must be an integer")
+
+        while True:
+            studentGender=input(f"Enter the Student's gender - M/F: \n").strip().upper()
+            if studentGender in ["M","F","MALE","FEMALE"]:
                 break
             print(f"Enter a valide gender entry - M or F or Male or Female ")
         #store dictionary details
@@ -38,4 +42,23 @@ while True:
         print(studentProfile)
 
     elif menu_pick=="2":
-        print(studentProfile)
+        if studentProfile:
+            print("\n--- STUDENT PROFILE ---")
+
+            for key, value in studentProfile.items():
+
+                print(f"{key}: {value}")
+
+        else:
+
+            print("No student profile found.")
+
+    elif menu_pick == "3":
+
+        print(f"Goodbye!")
+
+        break
+
+    else:
+
+        print("Invalid menu option. Try again.")
